5f0050fa81d3ab64edd1182b727f09837a7f8fef
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-11-05 23:33:14 +0100

    most of the layout done

---
+++ components/Generator.vue
@@ -0,0 +1,86 @@
+<template>
+  <section class="hero bg pb-6">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="columns is-centered">
+          <div class="column is-6">
+            <div class="card bg-card">
+              <div class="card-content">
+                <div class="content">
+                  <h5
+                    class="has-text-grey-light is-size-3 has-text-weight-light"
+                  >
+                    FwPY29RzAsGUo01B
+                  </h5>
+                  <div class="level">
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <vs-switch square val="css" color="#5e6ad2"
+                          >ABC</vs-switch
+                        >
+                      </div>
+                    </div>
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <vs-switch square val="css" color="#5e6ad2"
+                          >abc</vs-switch
+                        >
+                      </div>
+                    </div>
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <vs-switch square val="css" color="#5e6ad2"
+                          >123</vs-switch
+                        >
+                      </div>
+                    </div>
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <vs-switch square val="css" color="#5e6ad2"
+                          >#$&</vs-switch
+                        >
+                      </div>
+                    </div>
+                  </div>
+                  <div class="level">
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <vs-button
+                          color="#543fd7"
+                          style="min-width: 60px"
+                          animation-type="scale"
+                        >
+                          <i class="bx bxs-copy"></i>
+                          <template #animate>Copy</template>
+                        </vs-button>
+                      </div>
+                    </div>
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <vs-button
+                          icon
+                          circle
+                          color="#543fd7"
+                          animation-type="rotate"
+                        >
+                          <i class="bx bx-refresh"></i>
+                          <template #animate>
+                            <i class="bx bx-refresh"></i>
+                          </template>
+                        </vs-button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,137 +1,198 @@
 <template>
-  <div class="container">
-    <div>
-      <header class="content-logos">
-        <logo />
-        <span class="plus">+</span>
-        <VuesaxLogo />
-      </header>
-      <h1 class="title">Nuxt.js + Vuesax</h1>
-      <h2 class="subtitle">
-        <a href="https://vuesax.com/">Vuesax</a> is a framework of ui components
-        for <a href="https://vuejs.org/">Vuejs</a>, It was created to make new
-        interfaces that have a new trend and are visually beautiful
-      </h2>
-      <div class="links">
-        <h3 class="h3">Vuesax</h3>
-        <a
-          href="https://vuesax.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://discordapp.com/invite/9dsKtvB"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          Discord
-        </a>
-        <a
-          href="https://github.com/lusaxweb/vuesax"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <!-- Hero Landing -->
+    <section class="hero bg pt-6">
+      <div class="hero-body">
+        <div class="container has-text-centered-desktop has-text-left-mobile">
+          <div class="columns is-centered">
+            <div class="column is-12-mobile is-12-tablet is-8-desktop">
+              <h5 class="intro is-size-4">Introducing GridBit</h5>
+              <h1 class="mt-3 has-text-white has-text-weight-bold is-size-1">
+                Random Password Generator
+              </h1>
+              <p class="mt-4 has-text-grey-light is-size-4">
+                Linear helps streamline software projects, sprints, tasks, and
+                bug tracking. It's built for high-performance teams.
+              </p>
+              <vs-button
+                class="reset mt-6"
+                size="large"
+                color="#543fd7"
+                floating
+              >
+                <i class="bx bxs-down-arrow"></i>
+                &nbsp;Learn more</vs-button
+              >
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="links">
-        <h3 class="h3">Nuxt.js</h3>
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    </section>
+    <!-- Generator -->
+    <Generator />
+    <!-- Safety section -->
+    <section class="hero has-background-black-bis py-6">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-5">
+              <h1 class="is-size-1 has-text-white has-text-weight-semibold">
+                Safety.
+              </h1>
+              <p class="has-text-grey is-size-5 mt-5">
+                All passwords you create with GridBit are generated on your
+                computer.
+              </p>
+              <p class="has-text-grey is-size-5 mt-5">
+                They are not saved or shared anywhere else. Not even GridBit can
+                see the password you create.
+              </p>
+              <p class="has-text-grey is-size-5 mt-5">
+                Just be sure to close this page once you’ve generated and copied
+                your password to prevent anyone else from seeing it if they use
+                your computer.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
+    <!-- Randomness section -->
+    <section class="hero bg py-6">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-5">
+              <h1 class="is-size-1 has-text-white has-text-weight-semibold">
+                Randomness.
+              </h1>
+              <p class="has-text-grey-light is-size-5 mt-5">
+                People often use words or numbers that mean something to them.
+                The problem with this is that you end up with passwords that are
+                easy to guess.
+              </p>
+              <p class="has-text-grey-light is-size-5 mt-5">
+                And an ordinary desktop computer can test over a hundred million
+                passwords per second.
+              </p>
+              <p class="has-text-grey-light is-size-5 mt-5">
+                So don’t risk it. Use a random password generator to create
+                truly random passwords that even the strongest computers can’t
+                crack.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Singularity section -->
+    <section class="hero has-background-black-bis py-6">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-5">
+              <h1 class="is-size-1 has-text-white has-text-weight-semibold">
+                Singularity.
+              </h1>
+              <p class="has-text-grey is-size-5 mt-5">
+                Using the same password across multiple accounts is a huge
+                no-no.
+              </p>
+              <p class="has-text-grey is-size-5 mt-5">
+                If hackers learn the password to one of your accounts, they’ll
+                have your password for all the others, too.
+              </p>
+              <p class="has-text-grey is-size-5 mt-5">
+                So never recycle passwords. Use different, unique passwords for
+                every account.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Fragility section -->
+    <section class="hero bg py-6">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-5">
+              <h1 class="is-size-1 has-text-white has-text-weight-semibold">
+                Fragility.
+              </h1>
+              <p class="has-text-grey-light is-size-5 mt-5">
+                Remember, short passwords are weak passwords. They’re too easily
+                cracked.
+              </p>
+              <p class="has-text-grey-light is-size-5 mt-5">
+                For the best password security, you need a long, complex
+                password composed of a random string of characters.
+              </p>
+              <p class="has-text-grey-light is-size-5 mt-5">
+                You should also avoid common words and phrases to prevent a
+                dictionary attack.
+              </p>
+              <p class="has-text-grey-light is-size-5 mt-5">
+                Only then will you have a strong password.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Go Bact to top section -->
+    <section class="hero has-background-black-bis py-6">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-centered has-text-centered">
+            <div class="column is-8">
+              <h1 class="is-size-1 has-text-white has-text-weight-semibold">
+                Have you got it?
+              </h1>
+              <p class="has-text-grey is-size-5 mt-5">
+                I hope you already understand why using GridBit.
+              </p>
+              <vs-button
+                class="reset mt-6"
+                size="large"
+                color="#543fd7"
+                floating
+              >
+                <i class="bx bxs-up-arrow"></i>
+                &nbsp;Back To Top</vs-button
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuesaxLogo from '~/components/VuesaxLogo.vue'
+import Generator from '../components/Generator'
 
 export default {
   components: {
-    Logo,
-    VuesaxLogo,
+    Generator,
   },
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.reset {
+  margin: auto;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 55px;
-  color: #35495e;
-  letter-spacing: 1px;
-  text-transform: capitalize;
-  margin: 25px 0;
+.intro {
+  color: #543fd7;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 1.1rem;
-  color: #526488;
-  word-spacing: 2px;
-  padding-bottom: 15px;
-  max-width: 600px;
+.bg {
+  background: #1c1d1f;
 }
-
-.subtitle a {
-  font-weight: 500;
-  color: inherit;
-}
-
-.links {
-  padding-top: 15px;
-  margin-bottom: 20px;
-}
-
-.content-logos {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-width: 500px;
-}
-
-.plus {
-  font-size: 2.5rem;
-  margin: 15px;
-  color: #35495e;
-}
-
-.h3 {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-weight: 400;
-  margin: 10px;
+.bg-card {
+  background: linear-gradient(180deg, #28292c 0%, rgba(39, 40, 43, 0.82) 100%);
+  box-shadow: 0px 13px 40px rgba(0, 0, 0, 0.3), 0px 4px 4px rgba(0, 0, 0, 0.2);
+  border: none;
+  border-radius: 16px;
 }
 </style>

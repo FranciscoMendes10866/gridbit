17d66a1571736899bf30443bc0c21d4d11824742
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-11-05 13:44:20 +0100

    init commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap');
+* {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  word-spacing: 1.75px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
